dff38478af6e1c47b22e36ac61303a2886ba7f4c
Paul Morris <creative2012@hotmail.com>
2023-01-12 21:20:57 +0000

    added question structure

---
+++ assets/js/questions.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: 'Question 1',
+        answer: {
+            1: 'ans1',
+            2: 'ans2',
+            3: 'ans3',
+            4: "ans4"
+        }
+    },
+    {
+        question: 'Question 2',
+        answer: {
+            1: 'ans1',
+            2: 'ans2',
+            3: 'ans3',
+            4: "ans4"
+        }
+    },
+    {
+        question: 'Question 3',
+        answer: {
+            1: 'ans1',
+            2: 'ans2',
+            3: 'ans3',
+            4: "ans4"
+        }
+    },
+    {
+        question: 'Question 4',
+        answer: {
+            1: 'ans1',
+            2: 'ans2',
+            3: 'ans3',
+            4: "ans4"
+        }
+    }
+    
+]
