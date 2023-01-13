7405c91b1ef3e2c8d06ed90610f2e5dcc585c083
Paul Morris <creative2012@hotmail.com>
2023-01-13 21:58:35 +0000

    corrected highscores logic and commented

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -3,10 +3,13 @@ const orderedList = document.querySelector('#highscores');
 
 //check if scores in local storage
 let data = JSON.parse(localStorage.getItem('highScores'));
-let header =  document.createElement('div');
-header.innerHTML = 'Name<span>Score</span>'
-orderedList.appendChild(header);
+//if there is add scores to page
 if (data != null) {
+    //add header
+    let header =  document.createElement('div');
+    header.innerHTML = 'Name<span>Score</span>';
+    orderedList.appendChild(header);
+    //add each score
     data.forEach(function (item) {
         let listItem =  document.createElement('li');
         listItem.innerHTML = item.inital+'<span>'+item.score+'</span>'
