74098752e53fc6040e05195946c6ea26bd8676db
Paul Morris <creative2012@hotmail.com>
2023-01-13 01:05:49 +0000

    general housekeeping

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -17,14 +17,14 @@ var intervalID = null;
 var timeRemaining = 75;
 var questionIndex = 0;
 var scoreSave = {
-        inital: '',
-        score: 0
-    }
+    inital: '',
+    score: 0
+}
 
 
 
 //function to set base state
-function int(){
+function int() {
     startScreen.classList.add('hide');
     questions.classList.remove('hide');
     // timeRemaining = qArrLength * 10;
@@ -33,10 +33,10 @@ function int(){
 }
 
 //function to start the timer and initiate the quiz
-function timer(){
-    intervalID = setInterval(function (){
+function timer() {
+    intervalID = setInterval(function () {
         //check if time remaining and count down the timer
-        if(timeRemaining > 0){
+        if (timeRemaining > 0) {
             timeRemaining--;
             timeIndicator.textContent = timeRemaining;
         } else {
@@ -45,42 +45,42 @@ function timer(){
             endGame();
         }
 
-    },1000);
+    }, 1000);
 
     //start quiz
     runQuiz(questionIndex);
 }
 //function to run the quiz
-function runQuiz(q){
+function runQuiz(q) {
 
     choices.innerHTML = '';
     questionTitle.innerHTML = questionShuff[q].question;
     let qChoices = questionShuff[q].choices;
-    
-    for (var i = 0; i < 4; i++ ){
+
+    for (var i = 0; i < 4; i++) {
         //create button
-       let btn = document.createElement('button');
-       //set the text and class
-       btn.innerHTML = qChoices[i];
-       btn.classList.add('choice');
-       //check if this is the correct answer
-       if(questionShuff[q].correct == i +1){
-        //if so add a data attribute with 1 for true or if not 0 for false
-        btn.dataset.test = 1;
-       } else {
-        btn.dataset.test = 0;
-       }
-       //add to the page.
-       choices.appendChild(btn);
+        let btn = document.createElement('button');
+        //set the text and class
+        btn.innerHTML = qChoices[i];
+        btn.classList.add('choice');
+        //check if this is the correct answer
+        if (questionShuff[q].correct == i + 1) {
+            //if so add a data attribute with 1 for true or if not 0 for false
+            btn.dataset.test = 1;
+        } else {
+            btn.dataset.test = 0;
+        }
+        //add to the page.
+        choices.appendChild(btn);
 
     }
-    questionIndex ++;
-    
+    questionIndex++;
+
 
 }
 //function for wrong answer
-function reduceTime(){
-    if(timeRemaining > 11){
+function reduceTime() {
+    if (timeRemaining > 11) {
         timeRemaining -= 10;
         timeIndicator.textContent = timeRemaining;
     } else {
@@ -91,18 +91,18 @@ function reduceTime(){
 
 }
 //function to move to next question
-function next(){
-    if(questionIndex < qArrLength && timeRemaining > 0){
+function next() {
+    if (questionIndex < qArrLength && timeRemaining > 0) {
         runQuiz(questionIndex);
     } else {
-        if(timeRemaining > 0){
+        if (timeRemaining > 0) {
             clearInterval(intervalID);
         }
         endGame();
     }
 }
 //function to end the game
-function endGame(){
+function endGame() {
 
     questions.classList.add('hide');
     endScreen.classList.remove('hide');
@@ -110,13 +110,13 @@ function endGame(){
 
 }
 //function to handle button clicks
-function buttonHandler(button){
-    if(button.id == "start"){
+function buttonHandler(button) {
+    if (button.id == "start") {
         int();
     }
-    if(button.classList.contains('choice')){
+    if (button.classList.contains('choice')) {
         ///check if correct answer
-        if(button.dataset.test == 1){
+        if (button.dataset.test == 1) {
             correctAns.play();
             next();
         } else {
@@ -124,62 +124,69 @@ function buttonHandler(button){
             reduceTime();
         }
     }
-    if(button.id == "submit"){
-        //save inital and score
-        scoreSave.inital = initials.value;
-        scoreSave.score = timeRemaining;
-        var toStorage = [];
-        if(JSON.parse(localStorage.getItem('highScores')) != null){
-            let data = JSON.parse(localStorage.getItem('highScores'));
-            data.forEach(function(item) {
-                toStorage.push(item);
-            });
-        }
-        toStorage.push(scoreSave);
-        toStorage.sort(compare);
-        
-        localStorage.setItem('highScores',JSON.stringify(toStorage));
+    if (button.id == "submit") {
+        rememberScores();
     }
 
 
 }
-function compare( a, b ) {
-    if ( a.score > b.score ){
-      return -1;
+//function to save score to localStorage
+function rememberScores() {
+    //save inital and score
+    scoreSave.inital = initials.value;
+    scoreSave.score = timeRemaining;
+    var toStorage = [];
+    //check if old scores in local storage
+    if (JSON.parse(localStorage.getItem('highScores')) != null) {
+        let data = JSON.parse(localStorage.getItem('highScores'));
+        data.forEach(function (item) {
+            toStorage.push(item);
+        });
     }
-    if ( a.score < b.score ){
-      return 1;
+    toStorage.push(scoreSave);
+    toStorage.sort(compare);
+
+    localStorage.setItem('highScores', JSON.stringify(toStorage));
+
+}
+//function to sort scores into high score order
+function compare(a, b) {
+    if (a.score > b.score) {
+        return -1;
+    }
+    if (a.score < b.score) {
+        return 1;
     }
     return 0;
-  }
+}
 //function to shuffle questions
 function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
+    let currentIndex = array.length, randomIndex;
+
     // While remaining items
     while (currentIndex != 0) {
-  
-      // Pick a remaining item.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current item.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+
+        // Pick a remaining item.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current item.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
     }
-  
+
     return array;
-  }
+}
 
 //get wrapper that will contain all buttons
 const wrapper = document.querySelector('.wrapper');
 //and add event listener
-wrapper.addEventListener('click', function(event) {
+wrapper.addEventListener('click', function (event) {
     const button = event.target;
     const isButton = button.nodeName === 'BUTTON';
     if (!isButton) {
         return;
     } else {
-        buttonHandler(button);    
+        buttonHandler(button);
     }
 });
